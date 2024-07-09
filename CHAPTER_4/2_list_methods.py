1207ed7abeede9214d22bd07bab12cfab966b4e5
sanu-programmer <sanuk8137@gmail.com>
2024-07-09 22:45:52 +0530

    Tuple complete

---
+++ CHAPTER_4/2_list_methods.py
@@ -0,0 +1,13 @@
+l1 = [1,8,7,2,21,15] 
+l1.sort() #: updates the list to [1,2,7,8,15,21] 
+print(l1) 
+l1.reverse()#: updates the list to [15,21,2,7,8,1]
+print(l1)  
+l1.append(8)#: adds 8 at the end of the list
+print(l1)   
+l1.insert(3,8)#: This will add 8 at 3 index
+print(l1) 
+l1.pop(2)#: Will delete element at index 2 and return its value.
+print(l1)  
+l1.remove(21)#: Will remove 21 from the list. 
+print(l1) 
