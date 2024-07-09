4c1c4be0d652179ffb23800c35b42f531cbc1b34
sanu-programmer <sanuk8137@gmail.com>
2024-07-09 22:58:34 +0530

    sets and Dect done

---
+++ CHAPTER_5/1_dict.py
@@ -0,0 +1,10 @@
+d = {} # Empty dictionary
+marks = {
+    "Sanu": 100,
+    "Shivam": 56,
+    "som": 23
+}
+
+
+# print(marks, type(marks))
+print(marks["Sanu"])
---
+++ CHAPTER_5/2_dict_methods.py
@@ -0,0 +1,14 @@
+marks = {
+    "Sanu": 100,
+    "Shivam": 56,
+    "som": 23
+}
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+# marks.update({"Sanu": 99, "Renuka": 100})
+# print(marks)
+
+print(marks.get("Sanu2")) # Prints None
+print(marks["Sanu2"]) # Returns an error
