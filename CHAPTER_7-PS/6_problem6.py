59cf896143ca847f09c16e091ca9c45d9d6b2f6c
sanu-programmer <sanuk8137@gmail.com>
2024-07-27 23:57:47 +0530

    pattern que!

---
+++ CHAPTER_7-PS/6_problem6.py
@@ -0,0 +1,8 @@
+# Write a program to calculate the factorial of a given number using for loop.
+n = int(input("Enter the number:  "))
+
+fact = 1
+for i in range(1, n+1):
+    fact *= i
+
+print(f"The Factorial of  {n} is {fact}")
