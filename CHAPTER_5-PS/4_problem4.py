4d774b94a6b8a1600f69763d8fff8d7e8cced005
sanu-programmer <sanuk8137@gmail.com>
2024-11-02 16:41:01 +0530

    function done!

--- CHAPTER_5-PS/4_problem4.py
+++ CHAPTER_5-PS/4_problem4.py
@@ -1,7 +1,7 @@
-# 4. What will be the length of following set s: 
+# 4. What will be the length of following set s:
 s = set() 
 s.add(20) 
 s.add(20.0) 
-s.add('20') # length of s after these operations should be 3 , let's see
+s.add('20') #
+# length of s after these operations should be 3, let's see
 print(len(s))
-print(list(s))
